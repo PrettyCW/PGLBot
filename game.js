fde923959c07e55abb1927296e952bd9b61f4fdb
corbfon <corbin.fonville@gmail.com>
2020-10-20 14:50:18 -0500

    add styling

--- game.js
+++ game.js
@@ -1,37 +1,37 @@
 // Game Simulation Code below.
-function Player (name, rating, playstyle) {
-  this.name = name;
-  this.rating = rating;
-  this.playstyle = playstyle; // 0: striker, 1: guardian, 2: anything else
-  this.goals = 0;
+function Player(name, rating, playstyle) {
+  this.name = name
+  this.rating = rating
+  this.playstyle = playstyle // 0: striker, 1: guardian, 2: anything else
+  this.goals = 0
 }
 
-function Team (name, abbrev) {
-  this.name = name;
-  this.abbrev = abbrev;
-  this.players = [];
-  this.goals = 0;
+function Team(name, abbrev) {
+  this.name = name
+  this.abbrev = abbrev
+  this.players = []
+  this.goals = 0
 
   this.addPlayer = (player) => {
-    this.players.push(player);
+    this.players.push(player)
   }
 
   this.rating = () => {
-    let total = 0;
+    let total = 0
     for (let i = 0; i < this.players.length; ++i) {
-      total += this.players[i].rating;
+      total += this.players[i].rating
     }
-    return total;
+    return total
   }
 
   this.playerScoreChance = (playerIndex) => {
-    let player = this.players[playerIndex];
+    let player = this.players[playerIndex]
     if (player.playstyle == 0) {
-      return player.rating + (this.rating() / 10);
+      return player.rating + this.rating() / 10
     } else if (player.playstyle == 1) {
-      return player.rating - (this.rating() / 10);
+      return player.rating - this.rating() / 10
     } else {
-      return player.rating;
+      return player.rating
     }
   }
 }
@@ -41,73 +41,73 @@ let minimumOdds = 0.001
 
 let matchEvents = []
 
-function scoreEvent (team, player, time) {
-  let formattedTime = ""
+function scoreEvent(team, player, time) {
+  let formattedTime = ''
   if (time < 0) {
-    formattedTime += "+"
-    time = Math.abs(time);
+    formattedTime += '+'
+    time = Math.abs(time)
   }
-  formattedTime += Math.floor(time / 60);
-  formattedTime += ":";
-  if (time % 60 < 10) formattedTime += "0";
-  formattedTime += (time % 60);
-  matchEvents.push(formattedTime + " - " + player + " has scored for " + team);
+  formattedTime += Math.floor(time / 60)
+  formattedTime += ':'
+  if (time % 60 < 10) formattedTime += '0'
+  formattedTime += time % 60
+  matchEvents.push(formattedTime + ' - ' + player + ' has scored for ' + team)
 }
 
-function score (team, time) {
-  team.goals++;
-  let ratingAmount = team.playerScoreChance(0) + team.playerScoreChance(1) + team.playerScoreChance(2);
-  let rand = Math.floor(Math.random() * ratingAmount);
+function score(team, time) {
+  team.goals++
+  let ratingAmount = team.playerScoreChance(0) + team.playerScoreChance(1) + team.playerScoreChance(2)
+  let rand = Math.floor(Math.random() * ratingAmount)
   if (rand < team.playerScoreChance(0)) {
-    team.players[0].goals++;
-    scoreEvent(team.abbrev, team.players[0].name, time);
+    team.players[0].goals++
+    scoreEvent(team.abbrev, team.players[0].name, time)
   } else if (rand < team.playerScoreChance(0) + team.playerScoreChance(1)) {
-    team.players[1].goals++;
-    scoreEvent(team.abbrev, team.players[1].name, time);
+    team.players[1].goals++
+    scoreEvent(team.abbrev, team.players[1].name, time)
   } else {
-    team.players[2].goals++;
-    scoreEvent(team.abbrev, team.players[2].name, time);
+    team.players[2].goals++
+    scoreEvent(team.abbrev, team.players[2].name, time)
   }
 }
 
-function simGame (t1, t2) {
-  let time = 299;
-  let t1Odds = scoringOdds + ((t1.rating() - t2.rating()) * 0.00005);
-  let t2Odds = scoringOdds + ((t2.rating() - t1.rating()) * 0.00005);
+function simGame(t1, t2) {
+  let time = 299
+  let t1Odds = scoringOdds + (t1.rating() - t2.rating()) * 0.00005
+  let t2Odds = scoringOdds + (t2.rating() - t1.rating()) * 0.00005
 
-  if (t1Odds < minimumOdds) t1Odds = minimumOdds;
-  if (t2Odds < minimumOdds) t2Odds = minimumOdds;
+  if (t1Odds < minimumOdds) t1Odds = minimumOdds
+  if (t2Odds < minimumOdds) t2Odds = minimumOdds
 
   while (time > -1) {
-    let rand = Math.random();
+    let rand = Math.random()
     if (rand < t1Odds) {
-      score(t1, time);
+      score(t1, time)
     } else if (rand < t1Odds + t2Odds) {
-      score(t2, time);
+      score(t2, time)
     }
 
-    --time;
+    --time
   }
 
   if (t1.goals == t2.goals) {
-    let overtime = 0;
+    let overtime = 0
     while (true) {
-      let rand = Math.random();
+      let rand = Math.random()
       if (rand < t1Odds) {
-        score(t1, -overtime);
-        break;
+        score(t1, -overtime)
+        break
       } else if (rand < t1Odds + t2Odds) {
-        score(t2, -overtime);
-        break;
+        score(t2, -overtime)
+        break
       }
-      ++overtime;
+      ++overtime
     }
   }
 
-  matchEvents.push(t1.abbrev + ": " + t1.goals + " " + t2.abbrev + ": " + t2.goals);
+  matchEvents.push(t1.abbrev + ': ' + t1.goals + ' ' + t2.abbrev + ': ' + t2.goals)
 }
 
-exports.Team = Team;
-exports.Player = Player;
-exports.simGame = simGame;
-exports.matchEvents = matchEvents;
+exports.Team = Team
+exports.Player = Player
+exports.simGame = simGame
+exports.matchEvents = matchEvents
--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-const game = require('./game');
-const fs = require('fs');
-const Discord = require('discord.js');
-const Team = game.Team;
-const Player = game.Player;
+const game = require('./game')
+const fs = require('fs')
+const Discord = require('discord.js')
+const Team = game.Team
+const Player = game.Player
 
-const client = new Discord.Client();
+const client = new Discord.Client()
 
 client.on('ready', () => {
-  console.log("Bot intialized.");
+  console.log('Bot intialized.')
 })
 
-client.on('message', message => {
+client.on('message', (message) => {
   if (message.content == 'sim') {
-    let t1 = new Team("Ghost Gaming", "GG");
-    let t2 = new Team("Echo Fox", "FOX");
-    t1.addPlayer(new Player("AlphaKep", 50));
-    t1.addPlayer(new Player("Gimmick", 60));
-    t1.addPlayer(new Player("SquishyMuffinz", 70));
+    let t1 = new Team('Ghost Gaming', 'GG')
+    let t2 = new Team('Echo Fox', 'FOX')
+    t1.addPlayer(new Player('AlphaKep', 50))
+    t1.addPlayer(new Player('Gimmick', 60))
+    t1.addPlayer(new Player('SquishyMuffinz', 70))
 
-    t2.addPlayer(new Player("Andy", 50));
-    t2.addPlayer(new Player("hec", 60));
-    t2.addPlayer(new Player("ClayX", 71));
+    t2.addPlayer(new Player('Andy', 50))
+    t2.addPlayer(new Player('hec', 60))
+    t2.addPlayer(new Player('ClayX', 71))
 
-    game.simGame(t1, t2);
-    console.log(game.matchEvents);
-    message.channel.send(game.matchEvents);
+    game.simGame(t1, t2)
+    console.log(game.matchEvents)
+    message.channel.send(game.matchEvents)
   }
 })
 
 fs.readFile('token.txt', (err, data) => {
-  if (err) return console.log(err);
-  client.login(data.toString());
-});
+  if (err) return console.log(err)
+  client.login(data.toString())
+})
